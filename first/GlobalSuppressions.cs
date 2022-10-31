3190429ce32a8c15ecca6bd3b5a8007a89e82284
Victor <victor.oliveira473@etec.sp.gov.br>
2022-10-31 20:27:05 -0300

    testeaula31

---
+++ first/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Estilo", "IDE0054:Usar a atribuição composta", Justification = "<Pendente>", Scope = "member", Target = "~M:first.Program.Main(System.String[])")]
+
