f6a95575eb59742759e2baa4fabfe5f432c1e3a0
Victor <victor.oliveira473@etec.sp.gov.br>
2022-10-31 19:50:40 -0300

    aula31

---
+++ first/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int número1;
+            int número2;
+            int total;
+            int total2;
+            número1 = 10;
+            número2 = 20;
+            total = número1 + número2;
+            total2 = total + 2;
+            Console.WriteLine("Boa noite!");
+            Console.WriteLine("técnico");
+            Console.WriteLine(total);
+            Console.ReadKey();
+        }
+    }
+}
